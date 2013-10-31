5341b0b8387504efe0550755de6646294bee8b69
elliottm <m_elliott@live.com>
2013-10-31 19:01:50 +0000

    Working..

--- sudoku.rb
+++ sudoku.rb
@@ -5,6 +5,15 @@ require_relative './helpers/application.rb'
 
 set :sessions, secret: "i now hate sudoku "
 
+get '/' do
+  prepare_to_check_solution
+  generate_new_puzzle_if_necessary
+  @current_solution = session[:current_solution] || session[:puzzle]
+  @solution = session[:solution]
+  @puzzle = session[:puzzle]
+  erb :index
+end
+
 def generate_new_puzzle_if_necessary
   return if session[:current_solution]
   sudoku = random_sudoku
@@ -19,29 +28,18 @@ def prepare_to_check_solution
 end
 
 def random_sudoku
-    seed = (1..9).to_a.shuffle + Array.new(81-9, 0)
-    sudoku = Sudoku.new(seed.join)
-    sudoku.solve!
-    sudoku.to_s.chars
+  seed = (1..9).to_a.shuffle + Array.new(81-9, 0)
+  sudoku = Sudoku.new(seed.join)
+  sudoku.solve!
+  sudoku.to_s.chars
 end
 
-# this method removes some digits from the solution to create a puzzle
 def puzzle(sudoku)
-  # this method is yours to implement
   puzzle_board = sudoku.dup
   (0..80).to_a.sample(rand(80)).each do |index|
     puzzle_board[index]= 0
   end
   return puzzle_board
-end
-
-get '/' do
-  prepare_to_check_solution
-  generate_new_puzzle_if_necessary
-  @current_solution = session[:current_solution] || session[:puzzle]
-  @solution = session[:solution]
-  @puzzle = session[:puzzle]
-  erb :index
 end
 
 post '/' do
